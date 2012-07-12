41f8081d030beb2b0a2715578e13fc928b2cf809
Ben Kero <ben.kero@gmail.com>
2012-07-11 23:44:35 -0700

    initial commit of puppet module generate

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: haproxy
+#
+# This module manages haproxy
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class haproxy {
+
+
+}
